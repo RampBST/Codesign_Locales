ab03d38708d76826f043b28befd2a5322f877020
Ramp <ramp.clash@gmail.com>
2024-04-10 18:27:18 +0100

    change donatorshop to vipshop

---
+++ cd_vipshop/locales_ui.js
@@ -0,0 +1,65 @@
+// Use locales to adjust the UI to your match your language
+// ONLY change the value of the locale (the part between "")
+window['LOCALE'] = {
+    header: {
+        store_title: "VIP Shop",
+        store_subtitle:"Browse premium player content",
+        search_placeholder: "Search for products",
+        cart: "Checkout"
+    },
+    main: {
+        packages_available: "Packages available",
+        on_sale: "On sale",
+        new: "New",
+        all: "All",
+        add_product: "Add to cart",
+        confirm_input: "Confirm"
+    },
+    footer: {
+        balance: "Balance",
+        balance_subtitle: "Available to spend",
+
+        add_credits: "Add credits",
+
+        cart: "In cart",
+        cart_subtitle: "Total value of cart"
+    },
+    checkout: {
+        checkout: "Checkout",
+        no_packages: "There are no packages in Your cart.",
+        order: "Your order",
+        total: "Order total:",
+        agreement: "By purchasing the packages You are automatically agreeing to our Terms of Service.",
+        purchase: "Purchase",
+        no_credits: `<h3 class="fw-bold">Oh no!</h3> It looks like you haven't got enough credits to complete this purchase.<br/>Make adjustments to the cart or add more credits.`,
+        your_cart: "Your cart",
+        processing: "Your purchase is processing"
+    },
+    
+    // Categories based off the purchase type
+    // The values which match will be placed in the same category
+    categories: {
+        money: "Money",
+        item: "Items",
+        weapon: "Weapons",
+        vehicle: "Vehicles",
+        plate_change: "Vehicles",
+        phone_number: "Character",
+        character_name: "Character",
+        character_slot: "Character",
+        bundle: "Bundles",
+        random: "Mystery"
+    },
+    // The small vehicle UI that pops up on the left when they walk to the car
+    vehicle: {
+        speed: "Speed",
+        acceleration: "Acceleration",
+        brakes: "Brakes",
+        handling: "Handling",
+        purchase: "Press <kbd>E</kbd> to purchase this vehicle",
+        confirm_purchase: "Press <kbd class='bg-success text-light'>E</kbd> again to confirm the purchase!",
+        test: "Press <kbd>G</kbd> to test this vehicle",
+        stock: "in stock"
+    }
+
+}
